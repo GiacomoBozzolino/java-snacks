a813016ae60d37cdaeda35cd3f02cc1cc4c791ce
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-10 11:40:01 +0100

    snack1 completato

---
+++ src/org/lessons/java/Snack1.java
@@ -0,0 +1,21 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Snack1 {
+	public static void main(String[] Args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("inserisci un numero:");
+		String strNumber =in.nextLine();
+        int  number = Integer.valueOf(strNumber);
+        
+        in.close();
+        
+        if (number % 2 == 0) {
+        	System.out.print(number);
+        } else {
+        	System.out.print(number + 1);
+        }
+	}
+
+}
