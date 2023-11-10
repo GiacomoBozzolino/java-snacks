5ee8db74f3ab3082a928a9d4e737b0ab3161a480
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-10 15:19:30 +0100

    Snack7 terminato

---
+++ src/org/lessons/java/Snack7.java
@@ -0,0 +1,27 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Snack7 {
+	public static void main (String[] Args) {
+		
+        Scanner in = new Scanner(System.in);
+        System.out.print("Inserisci il numero di secondi: ");
+        int totaliSecondi = in.nextInt();
+        in.close();
+
+//     calcole le ore dai secondi 
+        int ore = totaliSecondi / 3600;
+//     calcolo i minuti dal resto dell'operazione precendete
+        int minuti = (totaliSecondi % 3600) / 60;
+//    calcolo i secondi dal resto dell'operazione precendete
+        int secondi = totaliSecondi % 60;
+
+//  String.format mi permette di formattare la stringa con zeri iniziali se necessario
+        String risultato = String.format("%02d:%02d:%02d", ore, minuti, secondi);
+
+       
+        System.out.println("Il risultato è: " + risultato);
+	}
+
+}
