6715aad98d759b7d2f77a18c488d943c836d0eef
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-10 15:23:56 +0100

    correzione errore battitura

--- src/org/lessons/java/Snack7.java
+++ src/org/lessons/java/Snack7.java
@@ -3,7 +3,7 @@ package org.lessons.java;
 import java.util.Scanner;
 
 public class Snack7 {
-	public static void main (String[] Args) {
+	public static void main (String[] args) {
 		
         Scanner in = new Scanner(System.in);
         System.out.print("Inserisci il numero di secondi: ");
