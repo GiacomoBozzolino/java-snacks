574f820532e9f0869b13de949cc30aba41e82937
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-10 12:12:06 +0100

    Snack3 completato

---
+++ src/org/lessons/java/Snack3.java
@@ -0,0 +1,16 @@
+package org.lessons.java;
+
+public class Snack3 {
+	public static void main (String[] Args) {
+		int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		int sommaPosizioneDispari = 0;
+		
+		for (int i = 1; i < numbers.length; i += 2) {
+            
+            sommaPosizioneDispari += numbers[i];
+        }
+		System.out.println("Somma degli elementi in posizione dispari: " + sommaPosizioneDispari);
+	}
+
+}
